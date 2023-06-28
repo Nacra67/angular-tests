1574bea406dbd62a4b8eb50cc2eac6a92a176e85
nacra67 <nacra67@gmail.com>
2023-06-28 10:07:57 +0200

    test_2

--- src/app/karma-jasmine.service.ts
+++ src/app/karma-jasmine.service.ts
@@ -20,5 +20,27 @@ export class KarmaJasmineService {
     return a / b;
   }
 
+  //   Función para verificar si un número es par
+  esPar(a: number): boolean {
+    return a % 2 == 0;
+  }
+  // Función para verificar si una cadena es un palíndromo
+  esPalindromo(palabra: string): boolean {
+    palabra = palabra.toLowerCase();
+    return palabra === palabra.split('').reverse().join('');
+  }
+  // Función para calcular el factorial de un número
+  factorial(numero: number): number {
+    if (numero === 0 || numero === 1) {
+      return 1;
+    } else {
+      let resultado = 1;
+      for (let i = 2; i <= numero; i++) {
+        resultado *= i;
+      }
+      return resultado;
+    }
+  }
+
 
 }
